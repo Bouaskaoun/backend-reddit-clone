2ea76586170801833c82f6aa5816df11bf9a2dd1
bouaskaoun <mohammed.bouaskaoun@edu.uca.ma>
2022-06-08 00:05:28 +0100

    first commit

---
+++ src/test/java/com/bouaskaoun/backendredditclone/BackendRedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bouaskaoun.backendredditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
